1400f0c60b812a103cc827023a9ad554aeafb5d4
joca <jpereyraleon@gmail.com>
2021-04-22 23:20:23 -0500

    first changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `DevJoca's Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Joca Pereyra`,
+      summary: `who lives and works in Lima building useful things.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    description: `A starter blog demonstrating what Gatsby can do :) .`,
+    siteUrl: `https://devjoca.me/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `devjoca`,
     },
   },
   plugins: [
--- src/components/bio.js
+++ src/components/bio.js
@@ -36,11 +36,11 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["AUTO", "WEBP", "AVIF"]}
-        src="../images/profile-pic.png"
-        width={50}
-        height={50}
+        src="../images/profile-pic.JPG"
+        width={100}
+        height={100}
         quality={95}
-        alt="Profile picture"
+        alt="Joca :)"
       />
       {author?.name && (
         <p>
